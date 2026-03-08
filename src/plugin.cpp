17c42e29a12449ec6a232eed2b8641eb3c9d5be7
Your Name <you@example.com>
2026-03-08 16:21:41 +0100

    Fix MetaModule build: SDK path override, INSTALL_DIR default, BTFLD slug, simulator registration

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,10 +1,18 @@
 #include "plugin.hpp"
 
 
+#ifdef METAMODULE_BUILTIN
+extern Plugin* pluginInstance;
+#else
 Plugin* pluginInstance;
+#endif
 
 
+#ifdef METAMODULE_BUILTIN
+void init_schlappiengineering(Plugin* p) {
+#else
 void init(Plugin* p) {
+#endif
 	pluginInstance = p;
 
 	// Add modules here
